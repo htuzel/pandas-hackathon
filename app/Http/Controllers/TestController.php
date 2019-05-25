75b129c4875f970cda6354781fb799534887f7cd
HasanEmektar <hasan.emektar@osf-commerce.com>
2019-05-25 13:18:42 +0300

    Conflict

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,26 +15,4 @@ class TestController extends Controller
         return $response;
     }
 
-    function test2 () {
-        $client = new Client();
-
-<<<<<<< HEAD
-        $response = $client->get('http://localhost:3004/projects?name=Squeaky Afterthought', [
-            'headers' => [
-                'Accept ' => 'application/json',
-                'Content-Type' => 'application/x-www-form-urlencoded'
-            ]
-        ]);
-
-        $results = json_decode($response->getBody(), true);
-=======
-        $res = Helper::db()->from('projects')
-                ->find('worklogs')
-                ->get();
-        dd($res);
->>>>>>> 3a1850fa713833ca58942b03407e2e76f8dae4fa
-
-        return $results;
-    }
-
 }
