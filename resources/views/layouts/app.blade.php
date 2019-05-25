1ee75f2b82d27d8cb70c23d9fa638c8f03cf5a58
Mehmet Uygun <mehmet.uygun@osf-commerce.com>
2019-05-25 17:57:12 +0300

    Added search result template and styles.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-        <!-- Scripts -->
+    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     @yield('content')
-    <!-- Scripts -->
 </body>
 </html>
