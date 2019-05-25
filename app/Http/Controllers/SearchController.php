e7e5fee9f40923d3d6c4e091cdda76203feadf0f
Hayreddin Tüzel <hayreddin.tuzel@osf-commerce.com>
2019-05-25 19:48:01 +0300

    refactoring

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,80 +14,17 @@ class SearchController extends Controller
         return view('search/index', compact('results'));
     }
 
-    public function search(SearchRequest $request)
+    public function search(Request $request)
     {
-        $resultArray = []; // Stores trimmed results
-        $searchQuery = $request->search_string;
-        $client = new Client();
-
-        $searchEngines = Helper::fixSearchString($searchQuery);
-
-        $response = $client->get('http://localhost:3004/projects', [
-            'headers' => [
-                'Accept ' => 'application/json',
-                'Content-Type' => 'application/x-www-form-urlencoded'
-            ]
-        ]);
-
-        $results = json_decode($response->getBody(), true);
-
-        foreach ($searchEngines as $searchEngine) {
-            foreach ($results as $result){
-                $issues = $result['issues'];
-                $projectArray = [];
-                $worklogArray = [];
-                $bugArray = [];
-                $writeAccess = false; // Ignore empty worklogs
-                $totalSpendTime = 0;
-                $projectArray['realTime'] = 0;
-                $projectArray['originalEstimate'] = 0;
-                $bugCounter = 0;
-                
-                if ($issues != null) {
-    
-                    foreach ($issues as $issue) {
-                        $issueComponenets = $issue['components'];
-    
-                        foreach ($issueComponenets as $issueComponenet) {
-                            if (!strcasecmp($issueComponenet, $searchEngine)) { // Ignoring case sensitive search
-                                if (count($issue['worklogs']) > 0) {
-                                    $writeAccess = true;
-                                    $projectArray['project'] = $result['name'];
-                                    $projectArray['originalEstimate'] += Helper::convertJiraTime($issue['originalEstimate'])->getData()->calculateSecond;
-    
-                                    foreach ($issue['worklogs'] as $item) {
-                                        $processedTime = Helper::convertJiraTime($item['timeSpent'])->getData();
-                                        $item['timeSpent'] = $processedTime->convertedTime;
-                                        $projectArray['realTime'] += $processedTime->calculateSecond;
-                                        
-                                        if ($issue['issueType'] != 'Bug') {
-                                            array_push($worklogArray, $item);
-                                            $projectArray['logs'] = $worklogArray;
-                                        } else {
-                                            $bugCounter++;
-                                            array_push($bugArray, $item);
-                                            $projectArray['bugs'] = $bugArray;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-    
-                    if ($writeAccess) {
-                        $projectArray['numberOfBugs'] = $bugCounter;
-                        $projectArray['componentName'] = $searchEngine;
-                        array_push($resultArray, $projectArray);
-                    }
-                }
-            }
-        }
+        $searchQuery = $request->input('search_string');
+        $resultArray = Helper::getSearchResults($searchQuery);
 
         return view('search/index', compact('searchQuery'))->with('resultArray', json_encode($resultArray));
     }
 
     public function recommendations(Request $request)
     {
+
         $componentNames = Helper::componentNames();
         $filtered = $componentNames->filter(function($value, $key) use ($request) {
             return stripos($value, $request->input('q')) !== false;
