6d01e78dc4863edcae6d87a8e77c6e8ca4fd9e34
HasanEmektar <hasan.emektar@osf-commerce.com>
2019-05-26 17:44:41 +0300

    Clear test components

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,6 @@
 |
 */
 
-
-//Generic testing routes
-Route::get('/test', 'TestController@test')->name('test');
-Route::get('/test2', 'TestController@test2')->name('test2');
-Route::get('/test3', 'TestController@test3')->name('test3');
-
 //API
 Route::get('/projects', 'ProjectController@projects')->name('projects');
 Route::get('/users', 'ProjectController@users')->name('users');
